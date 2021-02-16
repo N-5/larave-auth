f9c7446cfb37ea7bd7773b9cf2a8bc725662811a
N-5 <yfe63590@gmail.com>
2021-02-16 23:56:46 +0900

    add column

--- resources/views/mypage/edit.blade.php
+++ resources/views/mypage/edit.blade.php
@@ -2,33 +2,42 @@
 
 @section('content')
 <div class="container m-5">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">ユーザー登録内容の変更</div>
-        <div class="card-body">
-          <form method="POST" action="{{ action('Auth\UserController@update') }}">
-            <div class="form-group">
-              <label for="name">
-                名前
-              </label>
-              <div>
-                <input type="text" name="name" class="form-control" value="{{ $user->name }}">
-              </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ユーザー登録内容の変更</div>
+                <div class="card-body">
+                    <form method="POST" action="{{ action('Auth\UserController@update') }}">
+                        <div class="form-group">
+                            <label for="name">
+                                名前
+                            </label>
+                            <div>
+                                <input type="text" name="name" class="form-control" value="{{ $user->name }}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                email
+                            </label>
+                            <div>
+                                <input type="text" name="email" class="form-control" value="{{ $user->email }}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                profile
+                            </label>
+                            <div>
+                                <input type="text" name="profile" class="form-control" value="{{ $user->profile }}">
+                            </div>
+                        </div>
+                        <button type="submit" class="user-btn">変更</button>
+                        {{ csrf_field() }}
+                    </form>
+                </div>
             </div>
-            <div class="form-group">
-              <label for="email">
-                email
-              </label>
-              <div>
-                <input type="text" name="email" class="form-control" value="{{ $user->email }}">
-              </div>
-              <button type="submit" class="user-btn">変更</button>
-              {{ csrf_field() }}
-          </form>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 @endsection
